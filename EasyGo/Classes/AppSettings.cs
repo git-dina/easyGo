1b88a830af1c885fc4f7cae7fc0fd26e9ab201d5
git-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-10-15 20:26:18 +0300

    Update AppSettings.cs

--- EasyGo/Classes/AppSettings.cs
+++ EasyGo/Classes/AppSettings.cs
@@ -13,6 +13,9 @@ namespace EasyGo.Classes
     {
 
         public static ResourceManager resourcemanager;
+        public static ResourceManager resourcemanagerreport;
+        public static ResourceManager resourcemanagerAr;
+        public static ResourceManager resourcemanagerEn;
         // app version
         static public string CurrentVersion
         {
@@ -25,8 +28,38 @@ namespace EasyGo.Classes
         }
 
         public static string lang = "en";
+        public static string Reportlang = "en";
+        public static string invoice_lang = "en";
         public static bool menuState;
 
+        internal static string dateFormat = "ShortDatePattern";
+        internal static string timeFormat = "ShortTimePattern";
+        internal static string accuracy;
 
+        public static string Currency;
+        public static long CurrencyId;
+
+        #region company info
+        public static string logoImage;
+        public static string companyName;
+
+        public static string com_name_ar;
+        public static string com_address_ar;
+        public static string Email;
+        public static string Fax;
+        public static string Mobile;
+        public static string Address;
+        public static string Phone;
+
+        //social
+        public static string com_website;
+        public static string com_website_icon;
+        public static string com_social;
+        public static string com_social_icon;
+        #endregion
+
+        #region print
+        public static string show_header;
+        #endregion
     }
 }
