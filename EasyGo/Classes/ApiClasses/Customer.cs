9be5293c2e96deb2b56e556b17a7c9be56133378
yasinidlbi <yasinidlbi@gmail.com>
2023-10-17 22:54:23 +0300

    Update Customer.cs

--- EasyGo/Classes/ApiClasses/Customer.cs
+++ EasyGo/Classes/ApiClasses/Customer.cs
@@ -19,7 +19,7 @@ namespace EasyGo.Classes.ApiClasses
         public long CustomerId { get; set; }
         public string Name { get; set; }
         public string Code { get; set; }
-        public string Company { get; set; }
+        //public string Company { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
         public string Mobile { get; set; }
