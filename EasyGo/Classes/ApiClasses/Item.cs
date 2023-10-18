35af696fe473fdfb9477c97acc3641398d78e298
git-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-10-19 00:20:33 +0300

    item - itemUnit classes

---
+++ EasyGo/Classes/ApiClasses/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyGo.Classes.ApiClasses
+{
+    public class Item
+    {
+        #region Attributes
+        public long ItemId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public string Type { get; set; }
+        public string Image { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<int> Min { get; set; }
+        public Nullable<int> Max { get; set; }
+        public Nullable<int> CategoryId { get; set; }
+        public bool IsExpired { get; set; }
+        public int AlertDays { get; set; }
+        public bool IsTaxExempt { get; set; }
+        public Nullable<decimal> Taxes { get; set; }
+        public Nullable<int> MinUnitId { get; set; }
+        public Nullable<int> MaxUnitId { get; set; }
+        public Nullable<decimal> AvgPurchasePrice { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        #endregion
+    }
+}
---
+++ EasyGo/Classes/ApiClasses/ItemUnit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyGo.Classes.ApiClasses
+{
+    public class ItemUnit
+    {
+        #region Attributes
+        public long ItemUnitId { get; set; }
+        public Nullable<long> ItemId { get; set; }
+        public Nullable<int> UnitId { get; set; }
+        public Nullable<int> SubUnitId { get; set; }
+        public Nullable<int> UnitValue { get; set; }
+        public Nullable<bool> IsDefaultSale { get; set; }
+        public Nullable<bool> IsDefaultPurchase { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<decimal> Cost { get; set; }
+        public string Barcode { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<int> CreateUserId { get; set; }
+        public Nullable<int> UpdateUserId { get; set; }
+        public Nullable<decimal> purchasePrice { get; set; }
+        #endregion
+    }
+}
