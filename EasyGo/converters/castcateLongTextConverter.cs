8a9f9da3373aa02caa193c2472df0511188b885b
yasinidlbi <yasinidlbi@gmail.com>
2023-10-28 23:06:47 +0200

    converters

---
+++ EasyGo/converters/castcateLongTextConverter.cs
@@ -0,0 +1,36 @@
+﻿using EasyGo.Classes;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EasyGo.converters
+{
+    class castcateLongTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                string s = value as string;
+                if (s.Length > 20)
+                    s = s.Substring(0, 17) + "...";
+
+                return s;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
