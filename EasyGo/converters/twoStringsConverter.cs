a05650e80d7cb3e6330f4004c928fbec60ead578
git-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-11-04 14:52:20 +0200

    twoStringsConverter

---
+++ EasyGo/converters/twoStringsConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EasyGo.converters
+{
+    internal class twoStringsConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+
+                return values[0] + " " + values[1];
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+
+            string[] values = null;
+            if (value != null)
+                return values = value.ToString().Split(' ');
+            return values;
+        }
+    }
+}
