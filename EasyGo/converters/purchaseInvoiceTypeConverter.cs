f0fe6caa6f633521c4a06f1fd091e8a66002cfb2
yasinidlbi <yasinidlbi@gmail.com>
2023-11-14 23:03:47 +0300

    Create purchaseInvoiceTypeConverter.cs

---
+++ EasyGo/converters/purchaseInvoiceTypeConverter.cs
@@ -0,0 +1,42 @@
+﻿using EasyGo.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EasyGo.converters
+{
+    public class purchaseInvoiceTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                /*
+                switch (value)
+                {
+                    //مشتريات 
+                    case "p":
+                        value = AppSettings.resourcemanager.GetString("trPurchaseInvoice");
+                        break;
+                   
+                    default: break;
+                }
+                */
+                return value;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
