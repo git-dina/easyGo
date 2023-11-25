1fa42857ea54e9c37f6b5b94ce9459576610e1b2
git-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-11-25 15:17:32 +0200

    itemLocation class

---
+++ EasyGo/Classes/ApiClasses/ItemLocation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyGo.Classes.ApiClasses
+{
+    public class ItemLocation
+    {
+        #region Attributes
+        public long ItemLocId { get; set; }
+        public Nullable<int> LocationId { get; set; }
+        public Nullable<long> Quantity { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        public Nullable<System.DateTime> StartDate { get; set; }
+        public Nullable<System.DateTime> EndDate { get; set; }
+        public Nullable<long> ItemUnitId { get; set; }
+        public string Notes { get; set; }
+
+        //extra
+        public string ItemName { get; set; }
+        public string LocationName { get; set; }
+        public string SectionName { get; set; }
+        public string UnitName { get; set; }
+        public string ItemType { get; set; }
+        public bool IsFreeZone { get; set; }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
