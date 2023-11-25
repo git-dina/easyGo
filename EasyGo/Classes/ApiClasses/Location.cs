238cde830ac038e1b1bd0780e3ac36f149b72b42
git-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-11-25 15:13:44 +0200

    location - section classes

---
+++ EasyGo/Classes/ApiClasses/Location.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyGo.Classes.ApiClasses
+{
+    public class Location
+    {
+        #region Attributes
+        public int LocationId { get; set; }
+        public string x { get; set; }
+        public string y { get; set; }
+        public string z { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<int> SectionId { get; set; }
+        public string Notes { get; set; }
+        public Nullable<int> BranchId { get; set; }
+        public Nullable<bool> IsFreeZone { get; set; }
+
+        //extra
+        public string SectionName { get; set; }
+        public string LocationName { get; set; }
+        #endregion
+    }
+}
---
+++ EasyGo/Classes/ApiClasses/Section.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyGo.Classes.ApiClasses
+{
+    public class Section
+    {
+        #region Attributes
+        public int SectionId { get; set; }
+        public string Name { get; set; }
+        public Nullable<System.DateTime> CreateDate { get; set; }
+        public Nullable<System.DateTime> UpdateDate { get; set; }
+        public Nullable<long> CreateUserId { get; set; }
+        public Nullable<long> UpdateUserId { get; set; }
+        public Nullable<int> BranchId { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public string Notes { get; set; }
+        public Nullable<bool> IsFreeZone { get; set; }
+        #endregion
+    }
+}
