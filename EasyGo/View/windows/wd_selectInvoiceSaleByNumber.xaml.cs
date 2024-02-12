925a359a7f3e1342666d9c061c000eed5f0d8751
yasinidlbi <yasinidlbi@gmail.com>
2024-02-13 02:01:19 +0300

    add wd_selectInvoiceSaleByNumber

---
+++ EasyGo/View/windows/wd_selectInvoiceSaleByNumber.xaml.cs
@@ -0,0 +1,329 @@
+﻿using EasyGo.Classes.ApiClasses;
+using EasyGo.Classes;
+using netoaster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyGo.View.windows
+{
+    /// <summary>
+    /// Interaction logic for wd_selectInvoiceSaleByNumber.xaml
+    /// </summary>
+     public partial class wd_selectInvoiceSaleByNumber : Window
+    {
+        public wd_selectInvoiceSaleByNumber()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch { }
+        }
+        private void Btn_colse_Click(object sender, RoutedEventArgs e)
+        {
+            isOk = false;
+            this.Close();
+        }
+        private async void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (!_IsFocused)
+                {
+                    Control c = CheckActiveControl();
+                    if (c == null)
+                        tb_InvoiceNumber.Focus();
+                    _IsFocused = true;
+                }
+
+                HelpClass.StartAwait(grid_main);
+
+                TimeSpan elapsed = (DateTime.Now - _lastKeystroke);
+                if (elapsed.TotalMilliseconds > 150)
+                {
+                    _BarcodeStr = "";
+                }
+                string digit = "";
+                // record keystroke & timestamp 
+                if (e.Key >= Key.D0 && e.Key <= Key.D9)
+                {
+                    //digit pressed!         
+                    digit = e.Key.ToString().Substring(1);
+                    // = "1" when D1 is pressed
+                }
+                else if (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                {
+                    digit = e.Key.ToString().Substring(6); // = "1" when NumPad1 is pressed
+                }
+                else if (e.Key >= Key.A && e.Key <= Key.Z)
+                    digit = e.Key.ToString();
+                else if (e.Key == Key.OemMinus)
+                {
+                    digit = "-";
+                }
+                _BarcodeStr += digit;
+                _lastKeystroke = DateTime.Now;
+                // process barcode
+
+                if (e.Key.ToString() == "Return" && _BarcodeStr != "")
+                {
+                    await dealWithBarcode(_BarcodeStr);
+
+                    tb_InvoiceNumber.Text = _BarcodeStr;
+                    _BarcodeStr = "";
+                    _IsFocused = false;
+                    e.Handled = true;
+                }
+
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        public bool isOk { get; set; }
+        public SalesInvoice salesInvoice { get; set; }
+
+        SalesInvoice purchaseInvModel = new SalesInvoice();
+        public static List<string> requiredControlList = new List<string>();
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {//load
+            MainWindow.mainWindow.KeyDown += HandleKeyPress;
+            try
+            {
+                HelpClass.StartAwait(grid_main);
+                requiredControlList = new List<string> { "InvoiceNumber" };
+
+
+                translate();
+
+                controls = new List<Control>();
+                FindControl(this.grid_main, controls);
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void translate()
+        {
+            txt_title.Text = AppSettings.resourcemanager.GetString("trReturn");
+            MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_InvoiceNumber, AppSettings.resourcemanager.GetString("trInvoiceNumber"));
+
+            btn_select.Content = AppSettings.resourcemanager.GetString("trSelect");
+        }
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            MainWindow.mainWindow.KeyDown -= HandleKeyPress;
+        }
+        public void FindControl(DependencyObject root, List<Control> controls)
+        {
+            controls.Clear();
+            var queue = new Queue<DependencyObject>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                var control = current as Control;
+                if (control != null && control.IsTabStop)
+                {
+                    controls.Add(control);
+                }
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(current); i++)
+                {
+                    var child = VisualTreeHelper.GetChild(current, i);
+                    if (child != null)
+                    {
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+        }
+        private async void Btn_select_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                HelpClass.StartAwait(grid_main);
+
+                string barcode = tb_InvoiceNumber.Text;
+                await dealWithBarcode(barcode);
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        #region barcode
+        bool _IsFocused = false;
+        public List<Control> controls;
+        DateTime _lastKeystroke = new DateTime(0);
+        static private string _BarcodeStr = "";
+
+        public Control CheckActiveControl()
+        {
+            for (int i = 0; i < controls.Count; i++)
+            {
+                Control c = controls[i];
+                if (c.IsFocused)
+                {
+                    return c;
+                }
+            }
+            return null;
+        }
+        private async Task dealWithBarcode(string barcode)
+        {
+            int codeindex = barcode.IndexOf("-");
+            string prefix = "";
+            if (codeindex >= 0)
+                prefix = barcode.Substring(0, codeindex);
+            prefix = prefix.ToLower();
+            barcode = barcode.ToLower();
+
+            if (prefix == "p")
+            {
+                // dina
+                /*
+                salesInvoice = await purchaseInvModel.GetInvoiceToReturn(barcode, MainWindow.userLogin.UserId, MainWindow.branchLogin.BranchId);
+                if (salesInvoice != null)
+                {
+                    if (salesInvoice.InvoiceItems.Select(x => x.Quantity).Sum() > 0)
+                    {
+                        isOk = true;
+                        this.Close();
+                    }
+                    else
+                        Toaster.ShowWarning(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trAllQuantityReturned"), animation: ToasterAnimation.FadeIn);
+                }
+                else
+                    Toaster.ShowWarning(Window.GetWindow(this), message: AppSettings.resourcemanager.GetString("trNoInvoice"), animation: ToasterAnimation.FadeIn);
+            */
+            }
+
+
+
+            tb_InvoiceNumber.Clear();
+            tb_InvoiceNumber.Focus();
+        }
+
+        private async void Tb_InvoiceNumber_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+
+                HelpClass.StartAwait(grid_main);
+                if (e.Key == Key.Return)
+                {
+                    string barcode = tb_InvoiceNumber.Text;
+                    await dealWithBarcode(barcode);
+                }
+
+                HelpClass.EndAwait(grid_main);
+            }
+            catch (Exception ex)
+            {
+
+                HelpClass.EndAwait(grid_main);
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        #endregion
+        #region validate - clearValidate - textChange - lostFocus - . . . . 
+
+        string input;
+        decimal _decimal = 0;
+        private void Number_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+
+
+                //only  digits
+                TextBox textBox = sender as TextBox;
+                HelpClass.InputJustNumber(ref textBox);
+                if (textBox.Tag.ToString() == "int")
+                {
+                    Regex regex = new Regex("[^0-9]");
+                    e.Handled = regex.IsMatch(e.Text);
+                }
+                else if (textBox.Tag.ToString() == "decimal")
+                {
+                    input = e.Text;
+                    e.Handled = !decimal.TryParse(textBox.Text + input, out _decimal);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        private void Code_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            try
+            {
+                //only english and digits
+                Regex regex = new Regex("^[a-zA-Z0-9. -_?]*$");
+                if (!regex.IsMatch(e.Text))
+                    e.Handled = true;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+
+        }
+        private void Spaces_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                e.Handled = e.Key == Key.Space;
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
+
+        #endregion
+
+
+    }
+}
