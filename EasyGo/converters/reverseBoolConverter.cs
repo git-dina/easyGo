fe8c69156366d4e75f51f22b6c72eca8956990e0
yasinidlbi <yasinidlbi@gmail.com>
2024-02-12 23:20:13 +0300

    Create reverseBoolConverter.cs

---
+++ EasyGo/converters/reverseBoolConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EasyGo.converters
+{
+    class reverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if(value != null)
+                {
+                    bool _value = bool.Parse(value.ToString());
+                    return !_value;
+                }
+                    return value;
+            }
+            catch
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
