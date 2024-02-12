963421e76e06ba9a604c709415d459394d7bcef0
yasinidlbi <yasinidlbi@gmail.com>
2024-02-13 02:01:05 +0300

    add uc_itemCardWithPrice

---
+++ EasyGo/Template/uc_itemCardWithPrice.xaml.cs
@@ -0,0 +1,73 @@
+﻿using EasyGo.Classes;
+using EasyGo.Classes.ApiClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyGo.Template
+{
+    /// <summary>
+    /// Interaction logic for uc_itemCardWithPrice.xaml
+    /// </summary>
+     public partial class uc_itemCardWithPrice : UserControl
+    {
+        public uc_itemCardWithPrice()
+        {
+            InitializeComponent();
+        }
+        public Item item { get; set; }
+        public SolidColorBrush Color { get; set; }
+        private async void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = this;
+            await getItemImage();
+        }
+        public event RoutedEventHandler Click;
+        void onButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (this.Click != null)
+            {
+                this.Click(this, e);
+            }
+        }
+
+        private async Task getItemImage()
+        {
+            byte[] imageBuffer;
+
+            bool isModified = HelpClass.chkImgChng(item.Image, (DateTime)item.UpdateDate, Global.TMPItemFolder);
+            if (isModified && item.Image != "")
+                imageBuffer = await FillCombo.item.DownloadImage(item.Image); // read this as BLOB from your DB
+            else
+                imageBuffer = HelpClass.readLocalImage(item.Image, Global.TMPItemFolder);
+
+            if (imageBuffer != null)
+            {
+                var bitmapImage = new BitmapImage();
+                using (var memoryStream = new System.IO.MemoryStream(imageBuffer))
+                {
+                    bitmapImage.BeginInit();
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.StreamSource = memoryStream;
+                    bitmapImage.EndInit();
+                }
+
+
+                img_image.Source = bitmapImage;
+            }
+            //else
+            //    HelpClass.getLocalImg("Item", item.Image, img_image);
+        }
+    }
+}
